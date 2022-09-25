938f248b724a1f7b99f6789ce98caeb8ab5d5633
Victor-Desktop <vict702k@stud.kea.dk>
2022-09-25 16:56:43 +0200

    Changed index to contain my information

--- js2.js
+++
@@ -1,6 +0,0 @@
-var button = document.getElementById("allDivs");
-button.addEventListener('click',clickMe, true);
-
-function clickMe(e) {
-    document.getElementById("myPTag").innerHTML = "This is a message from " + e.target.id;
-}
